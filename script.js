d673d648f6701abb2c3e0db010d94fe281953e2e
jacopobiagi <jacopo.biagi02@gmail.com>
2023-11-29 19:30:54 +0100

    Inserimento del punteggio nella pagina e  blocco

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //DICHIARAZIONI
-let grid = document.getElementById("gridContainer");
 let start = document.getElementById("start");
 
 
@@ -10,11 +9,15 @@ start.addEventListener("click",
     ()=>{
         let cond = 0;
         let classe = "";
+        let grid = document.getElementById("gridContainer");
+        let score = document.querySelector(".punteggio");
+        let contatore = document.getElementById("contatore");
 
         let level = document.getElementById("selectLevel").value;
         grid.innerHTML = "";
         grid.classList.remove("d-none");
         grid.classList.add("d-flex");
+        score.classList.remove("d-none");
 
 
         //1 CASE: LIVELLO DIFFICIEL
@@ -39,35 +42,57 @@ start.addEventListener("click",
 
         let arrBoom = getRandomArr(1, cond, 16);
         let indexBoom = 0;
+        let points = 0;
+        contatore.innerHTML = points;
         console.log(arrBoom)
 
         for(let i = 1; i <= cond; i++){
 
             let element = document.createElement("div");
+            var bombaPresa = false;
+
             element.classList.add("content");
             element.classList.add(classe);    
             element.innerHTML = i;
 
             if(arrBoom[indexBoom] == i){
 
-                indexBoom++
+                indexBoom++;
 
                 element.addEventListener("click",
             
                     ()=>{
-                        console.log("hai cliccato l'elemento numero: "+ i);
-                        element.style.backgroundColor = "red";
+                        if(!bombaPresa){
+
+                            if(element.style.backgroundColor != "red"){
+
+                                console.log("hai cliccato l'elemento numero: "+ i);
+                                element.style.backgroundColor = "red";
+                                bombaPresa = true;
+                            }
+                        }
+                        //scopriBombe(grid, element, cond, arrBoom);
+                        
                     }
                 )
             }else{
 
                 element.addEventListener("click",
             
-                ()=>{
-                    console.log("hai cliccato l'elemento numero: "+ i);
-                    element.style.backgroundColor = "blue";
-                }
-            )
+                    ()=>{
+
+                        if(!bombaPresa){
+                            
+                            if(element.style.backgroundColor != "blue"){
+
+                                element.style.backgroundColor = "blue";
+                                points++;
+                                contatore.innerHTML = points;
+                                console.log("hai cliccato l'elemento numero: "+ i);
+                            }
+                        }
+                    }
+                )
 
             }
         
@@ -101,8 +126,19 @@ function getRandomArr(minNum, maxNum, arrLength){
 function getRandom(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+/*
+function scopriBombe(grid, element, cond, arrboom){
 
+    let index = 0;
 
+    for(let i = 0; i < cond; i++){
+
+        if(arrboom[index] == i){
+
+        }
+    }
+}
+*/
     
 
 
